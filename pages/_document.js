b719491d558b797c6d813ffd2131f3499b001ee3
Zakkom <zakkomisar@gmail.com>
2025-07-02 22:36:32 +0100

    Add custom _document.js for favicon and metadata

---
+++ pages/_document.js
@@ -0,0 +1,9 @@
+import Head from "next/head";
+
+...
+
+<Head>
+  <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+  <meta name="theme-color" content="#000000" />
+  <meta name="description" content="TechnoCore – Industrial Gas Solutions (Nitrogen, Oxygen, Filtration, Dryers)" />
+</Head>
